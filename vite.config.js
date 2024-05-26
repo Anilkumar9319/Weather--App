9cc2c5ce371b7ad3a5c73f306da117cbdd362daa
Anilkumar9319 <124231225+Anilkumar9319@users.noreply.github.com>
2024-05-26 16:03:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-project/",
-  build: {
-    main: './src/main.jsx', // specify the exact path to main.jsx
-  },
-});
+  base: "/React-project/"
+})
